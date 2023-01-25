a0605e07ed1651816c30cb6ec67aa233ede6ab42
hmetkaya <122322029+hmetkaya@users.noreply.github.com>
2023-01-25 23:17:02 +0100

    react-redux example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  const {counter} = useSelector(state=>state.counter)
+
+console.log(counter)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App my-10">
+      <button onClick={()=>{dispatch(decrement())}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+        -
+      </button>
+      <span className="mx-10">{counter}</span>
+     
+      <button onClick={()=>{dispatch(increment())}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+        +
+      </button>
     </div>
   );
 }
